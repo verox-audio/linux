7570cfc7ac2a56623f2efb21410180f9283a63d5
Spockfish <htenberge@gmail.com>
2026-02-27 10:13:36 +0100

    add quirk for HibY FC4

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -2082,6 +2082,7 @@ u64 snd_usb_interface_dsd_format_quirks(struct snd_usb_audio *chip,
 		break;
 
 	case USB_ID(0x4102, 0x1223): /* Astell & Kern PEE51 USB DAC */
+	case USB_ID(0x32bb, 0x0004): /* HiBy FC4 */
 		if (fp->altsetting == 4)
 			return SNDRV_PCM_FMTBIT_DSD_U32_BE;
 		break;
